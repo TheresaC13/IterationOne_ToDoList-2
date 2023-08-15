b737c45a924c978c62a3f4f96bbc18c075bcd55f
Theresa <tcullen828@gmail.com>
2023-08-15 10:50:41 -0400

    To Do List It. 1

---
+++ IterationOne_ToDoList/ToDoItem.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoItem.swift
+//  IterationOne_ToDoList
+//
+//  Created by scholar on 6/20/23.
+//
+
+//** This file contains all the code for the ToDoItem class**
+
+import Foundation
+
+//Change the class to ToDoItem: Identifiable
+class ToDoItem: Identifiable {
+  
+    //Add the variable id and set it equal to type UUID()
+    var id = UUID()
+    var title = ""
+    var isImportant = false
+    
+    init(title: String, isImportant: Bool = false) {
+        self.title = title
+        self.isImportant = isImportant
+    }
+}
